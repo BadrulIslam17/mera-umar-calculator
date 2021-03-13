7be84d9b20bf65680955fdb0c96ce4d0a86cdba0
ishafeeq <78305827+BadrulIslam17@users.noreply.github.com>
2021-03-13 16:43:08 +0530

    finalllllyyy!
    cheating x100

---
+++ umar-calc/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This Is Bhadlul</h1>
+          <img
+            src={'./builali.jpg'}
+            alt="Giveup"
+            className="image"
+            width="220px"
+            height="200px"
+            />
+      </div>
+    );
+  }
+}
